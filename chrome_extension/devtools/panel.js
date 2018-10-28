9b86c1959a445f150e1455d86ff53eef40633bff
vtst <git@vtst.net>
2018-10-28 18:24:28 +0100

    Setting up AngularJS Material in the panel

--- chrome_extension/devtools/panel.js
+++ chrome_extension/devtools/panel.js
@@ -15,9 +15,9 @@ w2j.panel.formatElementInfo = function(elementInfo) {
 };
 
 w2j.panel.init = async function() {
-  await w2j.utils.onContentLoaded();
   var messageDiv = document.getElementById('message');
   async function updateElementInfo() {
+    console.log('update');
     var response = await w2j.utils.evalScriptInInspectedWindow('devtools/panel_cs.js');
     messageDiv.textContent = 
       w2j.utils.map(response.result, w2j.panel.formatElementInfo).join('\n');
@@ -26,4 +26,12 @@ w2j.panel.init = async function() {
   chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementInfo);
 };
 
-w2j.panel.init();
+// *************************************************************************
+// AngularJS app and controller
+
+var module = angular.module('PanelApp', ['ngMaterial', 'ngMessages']);
+
+module.controller('PanelCtrl', function($scope) {
+  w2j.panel.init();
+});
+
