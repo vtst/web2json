4792118169da6187fd01ed285b95b38eca35314e
Vincent Simonet <github.self@vtst.net>
2018-10-28 11:39:49 +0100

    Format out of eval

--- chrome_extension/devtools/panel.js
+++ chrome_extension/devtools/panel.js
@@ -1,12 +1,26 @@
 var w2j = w2j || {};
 w2j.panel = {};
 
+/**
+@param {w2j.panel.ElementInfo}
+@return {string}
+*/
+w2j.panel.formatElementInfo = function(elementInfo) {
+  var s = elementInfo.tagName;
+  if (elementInfo.id) s += '#' + elementInfo.id;
+  w2j.utils.forEach(elementInfo.classNames, className => {
+    s += '.' + className;
+  });
+  return s;
+};
+
 w2j.panel.init = async function() {
   await w2j.utils.onContentLoaded();
   document.getElementById('message').textContent = 'Hello!';
   chrome.devtools.panels.elements.onSelectionChanged.addListener(async function(event) {
     var response = await w2j.utils.evalScriptInInspectedWindow('devtools/panel_cs.js');
-    document.getElementById('message').textContent = response.result.join('\n');
+    document.getElementById('message').textContent = 
+      w2j.utils.map(response.result, w2j.panel.formatElementInfo).join('\n');
   });
 };
 
--- chrome_extension/devtools/panel_cs.js
+++ chrome_extension/devtools/panel_cs.js
@@ -3,20 +3,33 @@ w2j.panel_cs = {};
 
 /**
 @param {Element} element
-@return {string}
+@return {Array.<string>}
 */
-w2j.panel_cs.inspectElement = function(element) {
-  var s = element.tagName.toLowerCase();
-  if (element.id) s += '#' + element.id;
-  if (element.classList) {
-    for (var i = 0; i < element.classList.length; ++i) {
-      s += '.' + element.classList.item(i);
-    }
+w2j.panel_cs.getClassNames = function(element) {
+  var classNames = [];
+  for (var i = 0; i < element.classList.length; ++i) {
+    classNames.push(element.classList.item(i));
   }
-  return s;
+  return classNames;  
 };
 
-w2j.panel_cs.inspectAncestors = function(element) {
+/**
+@param {Element} element
+@return {w2j.panel.ElementInfo}
+*/
+w2j.panel_cs.inspectElement = function(element) {
+  return {
+    tagName: element.tagName.toLowerCase(),
+    classNames: w2j.panel_cs.getClassNames(element),
+    id: element.id
+  };
+};
+
+/**
+@param {Element} element
+@return {Array.<w2j.panel.ElementInfo>}
+*/
+w2j.panel_cs.inspectAncestorElements = function(element) {
   var current = element;
   var result = [];
   while (current instanceof Element) {
@@ -26,4 +39,4 @@ w2j.panel_cs.inspectAncestors = function(element) {
   return result;
 };
 
-return w2j.panel_cs.inspectAncestors($0);
+return w2j.panel_cs.inspectAncestorElements($0);
