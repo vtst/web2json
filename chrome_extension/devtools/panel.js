3525deb0ff38ca687017c56a542583eb6dec9e48
Vincent Simonet <github.self@vtst.net>
2018-11-01 10:41:15 +0100

    Delete dead code

--- chrome_extension/devtools/panel.js
+++ chrome_extension/devtools/panel.js
@@ -1,19 +1,6 @@
 var w2j = w2j || {};
 w2j.panel = {};
 
-/**
-@param {w2j.panel.ElementInfo}
-@return {string}
-*/
-w2j.panel.formatElementInfo = function(elementInfo) {
-  var s = elementInfo.tagName;
-  if (elementInfo.id) s += '#' + elementInfo.id;
-  w2j.utils.forEach(elementInfo.classNames, className => {
-    s += '.' + className;
-  });
-  return s;
-};
-
 w2j.panel.init = async function($scope) {
   var messageDiv = document.getElementById('message');
   async function updateElementInfo() {
