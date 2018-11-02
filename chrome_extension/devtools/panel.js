abf232ca40441b8b38accf5f71d329dfa34e1d0e
Vincent Simonet <github.self@vtst.net>
2018-11-02 17:18:40 +0100

    Show number of elements

--- chrome_extension/devtools/panel.js
+++ chrome_extension/devtools/panel.js
@@ -1,52 +1,124 @@
 var w2j = w2j || {};
 w2j.panel = {};
 
-w2j.panel.getSelectableElementInfo = function(elementInfo) {
-  function getSelectable(value) {
-    return {
-      selected: false,
-      value: value
-    };
-  }
+// *************************************************************************
+// Utilities for managing ElementInfo
+
+/**
+@typedef {
+  tagName: string,
+  id: string,
+  classNames: Array.<string>
+}
+*/
+w2j.panel.ElementInfo;
+
+/** @typedef {selected: boolean, value: string} */
+w2j.panel.Selectable;
+
+/**
+@typedef {
+  tagName: w2j.panel.Selectable,
+  id: w2j.panel.Selectable,
+  classNames: Array.<w2j.panel.Selectable>
+}
+*/
+w2j.panel.SelectableElementInfo;
+
+/**
+@param {string} value
+@return {w2j.panel.Selectable}
+*/
+w2j.panel.getSelectable = function(value) {
   return {
-    tagName: getSelectable(elementInfo.tagName),
-    id: getSelectable(elementInfo.id),
-    classNames: w2j.utils.map(elementInfo.classNames, getSelectable)
+    selected: false,
+    value: value
   };
 };
 
-w2j.panel.init = async function($scope) {
-  var messageDiv = document.getElementById('message');
-
-  // Element selection
-  async function updateElementInfo() {
-    var response = await w2j.utils.evalScriptInInspectedWindow('devtools/panel_eval.js');
-    response.result.reverse();
-    $scope.selectableAncestorInfos = w2j.utils.map(response.result, w2j.panel.getSelectableElementInfo);
-    $scope.$apply();
-  }
-  updateElementInfo();
-  chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementInfo);
-
-  // Page navigation
-  async function injectScripts() {
-    await chromp.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {file: 'utils.js'});
-    await chromp.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {file: 'devtools/panel_cs.js'});    
-  }
-  chrome.devtools.network.onNavigated.addListener(injectScripts);
-  injectScripts();
+/**
+@param {w2j.panel.ElementInfo} elementInfo
+@return {w2j.panel.SelectableElementInfo}
+*/
+w2j.panel.getSelectableElementInfo = function(elementInfo) {
+  return {
+    tagName: w2j.panel.getSelectable(elementInfo.tagName),
+    id: w2j.panel.getSelectable(elementInfo.id),
+    classNames: w2j.utils.map(elementInfo.classNames, w2j.panel.getSelectable)
+  };
 };
+
+/**
+@param {w2j.panel.SelectableElementInfo}
+@return {string}
+*/
+w2j.panel.formatSelectableAncestorInfo = function(selectableAncestorInfo) {
+  var s = '';
+  if (selectableAncestorInfo.tagName.selected) s += selectableAncestorInfo.tagName.value;
+  if (selectableAncestorInfo.id.selected) s += '#' + selectableAncestorInfo.id.value;
+  w2j.utils.forEach(selectableAncestorInfo.classNames, function(className) {
+    if (className.selected) s += '.' + className.value;
+  });
+  return s;
+}
+
+/**
+@param {Array.<w2j.panel.SelectableElementInfo>}
+@return {string}
+*/
+w2j.panel.formatSelectableAncestorInfos = function(selectableAncestorInfos) {
+  return w2j.utils.filter(
+    w2j.utils.map(selectableAncestorInfos, w2j.panel.formatSelectableAncestorInfo),
+    function(s) { return s; }
+    ).join('\n');
+}
+
+// *************************************************************************
+// Interaction with inspected window
 
 w2j.panel.sendMessage = async function(message) {
   return await chromp.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, message);
 };
 
-w2j.panel.querySelectorAll = async function(selector) {
+w2j.panel.getNumberOfMatches = async function(selector) {
+  if (!selector) return 0;
   var response = await w2j.panel.sendMessage({
     _w2j_: 'querySelectorAll',
     selector: selector
   });
   return response.numberOfElements;
+};
+
+// *************************************************************************
+// Page interaction
+
+/**
+@param {Scope} $scope
+*/
+w2j.panel.onSelectionChanged = async function($scope) {
+  var response = await w2j.utils.evalScriptInInspectedWindow('devtools/panel_eval.js');
+  response.result.reverse();
+  $scope.selectableAncestorInfos = w2j.utils.map(response.result, w2j.panel.getSelectableElementInfo);
+  $scope.$apply();
+};
+
+w2j.panel.onNavigated = async function() {
+  await chromp.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {file: 'utils.js'});
+  await chromp.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {file: 'devtools/panel_cs.js'});    
+};
+
+/**
+@param {Scope} $scope
+*/
+w2j.panel.init = async function($scope) {
+  // Element selection
+  chrome.devtools.panels.elements.onSelectionChanged.addListener(
+    w2j.panel.onSelectionChanged.bind(null, $scope));
+  w2j.panel.onSelectionChanged($scope);
+
+  // Page navigation
+  chrome.devtools.network.onNavigated.addListener(w2j.panel.onNavigated);
+  w2j.panel.onNavigated();
 };
 
 // *************************************************************************
@@ -71,31 +143,9 @@ module.component('w2jSelectorItem', {
 module.controller('PanelCtrl', function($scope) {
   w2j.panel.init($scope);
 
-  function formatSelectableAncestorInfo(selectableAncestorInfo) {
-    var s = '';
-    console.log(selectableAncestorInfo);
-    if (selectableAncestorInfo.tagName.selected) s += selectableAncestorInfo.tagName.value;
-    if (selectableAncestorInfo.id.selected) s += '#' + selectableAncestorInfo.id.value;
-    w2j.utils.forEach(selectableAncestorInfo.classNames, function(className) {
-      if (className.selected) s += '.' + className.value;
-    });
-    if (s) s += '\n';
-    return s;
-  }
-
-  function formatSelectableAncestorInfos(selectableAncestorInfos) {
-    return w2j.utils.map(selectableAncestorInfos || [], formatSelectableAncestorInfo).join('');
-  }
-
-  $scope.formatSelection = function(selectableAncestorInfos) {
-    var strings = w2j.utils.map(selectableAncestorInfos || [], formatSelectableAncestorInfo);
-    strings.reverse();
-    return strings.join('');
-  }
-
-  $scope.test = async function() {
-    var selector = formatSelectableAncestorInfos($scope.selectableAncestorInfos);
-    var n = await w2j.panel.querySelectorAll(selector);
-    alert(n + ' element(s) match(es) this selector');
-  };
+  $scope.$watch('selectableAncestorInfos', async function(newValue, oldValue) {
+    var selector = w2j.panel.formatSelectableAncestorInfos($scope.selectableAncestorInfos || []);
+    $scope.numberOfMatches = await w2j.panel.getNumberOfMatches(selector);
+    $scope.$apply();
+  }, true);
 });
