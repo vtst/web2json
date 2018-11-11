9f0d72d77f0bbd1a465b972209f4c6336b4c3a76
Vincent Simonet <github.self@vtst.net>
2018-11-11 20:40:42 +0100

    Skeleton of editor

--- chrome_extension/editor/index.js
+++ chrome_extension/editor/index.js
@@ -1,3 +1,18 @@
-var module = angular.module('EditorApp', ['ngMaterial', 'ngMessages']);
+var module = angular.module('EditorApp', ['ngMaterial', 'ngMessages', 'ui.codemirror']);
 
-module.controller('EditorCtrl', function($scope) {});
+function initCodeMirror(textArea) {
+  var timeout = {};
+  timeout.id = window.setInterval(function() {
+    if (!textArea.classList.contains('ng-cloak')) {
+      window.clearInterval(timeout.id);
+      CodeMirror.fromTextArea(textArea, {
+        lineNumbers: true
+      });
+    }
+  }, 10);
+}
+
+module.controller('EditorCtrl', function($scope) {
+  var textArea = document.getElementById('editor');
+  initCodeMirror(textArea);
+});
