1cd45c258cf973783d9d7921f12225c392d687f2
Vincent Simonet <github.self@vtst.net>
2018-11-18 19:24:19 +0100

    Codemirror initialization

--- chrome_extension/editor/index.js
+++ chrome_extension/editor/index.js
@@ -1,19 +1,45 @@
-var module = angular.module('EditorApp', ['ngMaterial', 'ngMessages', 'ui.codemirror']);
+var w2j = w2j || {};
+w2j.editor = {};
 
-function initCodeMirror(textArea) {
-  var timeout = {};
-  timeout.id = window.setInterval(function() {
-    if (!textArea.classList.contains('ng-cloak')) {
-      window.clearInterval(timeout.id);
-      CodeMirror.fromTextArea(textArea, {
+var module = angular.module('EditorApp', ['ngMaterial', 'ngMessages']);
+
+module.controller('EditorCtrl', function($scope) {
+});
+
+w2j.editor.initCodeMirror = function(element, options) {
+  var observer = new MutationObserver(function(mutations, observer) {
+    w2j.utils.forEach(mutations, function(mutation) {
+      if (mutation.attributeName == 'class' &&
+        mutation.oldValue && mutation.oldValue.includes('ng-cloak') && !mutation.target.classList.contains('ng-cloak')) {
+        window.setTimeout(function() {
+          CodeMirror.fromTextArea(element, options);
+        }, 0);
+        observer.disconnect();
+      }
+    });
+  });
+  observer.observe(document.body, {
+    subtree: false,
+    attributes: true,
+    attributeOldValue: true
+  });
+};
+
+module.directive("w2jCodemirror", function () {
+  return {
+    restring: 'A',
+    compile: function(elements, attrs) {
+      w2j.editor.initCodeMirror(elements[0], {
         lineNumbers: true,
         viewportMargin: Infinity
       });
+    },
+    bindToController: true,
+    controller: function($element, $timeout) {
+      $timeout(function() {
+      }, 1000);
+      this.$postLink = function() {
+      }
     }
-  }, 10);
-}
-
-module.controller('EditorCtrl', function($scope) {
-  var textArea = document.getElementById('editor');
-  initCodeMirror(textArea);
+  };
 });
